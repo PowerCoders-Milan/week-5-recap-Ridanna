6a2cb262416d07e2e63e684673da9350a2f5c033
mahmudref@gmail.com <mahmudref@gmail.com>
2021-03-05 16:53:35 +0100

    exe2dom

---
+++ dom-ex3/exercise.js
@@ -0,0 +1,12 @@
+function mySparkle () {
+    var click = click++
+    document.getElementsByTagName('button')
+    if (click === 1) {
+        button.style.backgroundColor = 'red'
+    } else if (click === 2){
+        button.style.backgroundColor = 'blue'
+    } else (click === 3) {
+        button.style.backgroundColor = 'white'
+    }
+}
+mySparkle()
---
+++ js-ex2/exercise.js
@@ -0,0 +1,8 @@
+var array1 = ['marco', 'antonio', 'luca', 13, 18, 'leo', 'mary'];
+var array2 = [21, 19, 12, 'sebastian', 'will', 23, 20];
+var arrray3 = array1 + array2;
+
+var myData = arrray3[[0][8]];
+
+
+
